22f45aa213939cb8ef3f82322db7962b8e8b26ce
Hawi Girmachew <88828065+Hawigirmachew@users.noreply.github.com>
2022-04-12 11:17:03 -0700

    Update runner-up.py

--- runner-up.py
+++ runner-up.py
@@ -2,7 +2,6 @@ from tkinter import Tk, ttk
 from tkinter import * 
 import random
 
-
 root = Tk()
 root.title("Python game")
 root.geometry("400x400")
@@ -15,7 +14,6 @@ attempt = 5
 
 def compare_and_check():
     global attempt
-
     user_input = number.get()
 
     if attempt > 0:
@@ -33,38 +31,30 @@ def compare_and_check():
         msg="Sorry you  lost the game"
     display.set(msg)    
 
-#title 
 Label(
-    root,
-    text="Number Guessing Game",
-    font="Times 20",
-    fg="#000",
+    root,text="Number Guessing Game",
+    font="Times 20",fg="#000",
     relief="raised",
-    #bg="black"
 ).pack(pady=10)
+
 Label(
-    root,
-    text="Enter a number between 1 and 10",
-    font ="Times 20",
-    relief="flat",
+    root,text="Enter a number between 1 and 10",
+    font ="Times 20",relief="flat",
     fg="#000",
-    #bg="#000"
 ).pack(pady=5)
+
 Entry(
-    root,
-    textvariable= number,
+    root, textvariable= number,
     font=("san sarif",20)
 ).pack(pady=(30,10)) 
+
 Button(
-    root,
-    text="Sumbit Guess",
-    font=("Times 20"),
-    command= compare_and_check
+    root, text="Sumbit Guess",
+    font=("Times 20"),command= compare_and_check
 ).pack(pady=30)
 
 Label(
-    root,
-    font=("helvetica 16"),
+    root,font=("helvetica 16"),
     textvariable= display
 ).pack(pady=(10,5))
 
